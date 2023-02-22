82dfb831ee2c163a34672fcbc17e5db352ed0b16
Skyrous <balan.claudiu96@gmail.com>
2023-02-22 16:01:41 +0200

    Added constructor and Annotations for other needed stuff (using Lombok)

--- src/main/java/com/loginapp/demo/userforapp/UserForApp.java
+++ src/main/java/com/loginapp/demo/userforapp/UserForApp.java
@@ -1,13 +1,30 @@
 package com.loginapp.demo.userforapp;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.Collections;
-
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
 public class UserForApp implements UserDetails {
+    public UserForApp(String name, String userName, String email, String password, AppUserRole appUserRole, Boolean locked, Boolean enabled) {
+        this.name = name;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.appUserRole = appUserRole;
+        this.locked = locked;
+        this.enabled = enabled;
+    }
+
     private Long id;
     private String name;
     private String userName;
